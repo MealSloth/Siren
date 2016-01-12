91c006c8af750e779c10ce2d5ceb2af5e8573dd2
Michael <michael@mealsloth.com>
2016-01-12 16:37:39 -0500

    Navigable pages

--- Siren/urls.py
+++ Siren/urls.py
@@ -5,6 +5,11 @@ from Siren import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='home'),
+    url(r'^blog', views.blog, name='blog'),
+    url(r'^about', views.about, name='about'),
+    url(r'^contact', views.contact, name='contact'),
+    url(r'^consumer', views.consumer, name='consumer'),
+    url(r'^producer', views.producer, name='producer'),
     url(r'^(?P<arg>[0-9A-Z]+)/$', views.detail, name='detail'),
     url(r'^test/(?P<arg>[0-9A-Z]+)/$', views.results, name='results'),
 ) + staticfiles_urlpatterns()
--- Siren/views.py
+++ Siren/views.py
@@ -1,12 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
     response = render(request, 'home.html')
     return HttpResponse(response)
 
+
+def about(request):
+    response = render(request, 'about.html')
+    return HttpResponse(response)
+
+
+def blog(request):
+    response = render(request, 'blog.html')
+    return HttpResponse(response)
+
+
+def contact(request):
+    response = render(request, 'blog.html')
+    return HttpResponse(response)
+
+
+def consumer(request):
+    response = render(request, 'consumer.html')
+    return HttpResponse(response)
+
+
+def producer(request):
+    response = render(request, 'producer.html')
+    return HttpResponse(response)
+
+
 def detail(request, arg):
     return HttpResponse("You're looking at question %s." % arg)
+
 
 def results(request, arg):
     response = "You're looking at the results of question %s."
